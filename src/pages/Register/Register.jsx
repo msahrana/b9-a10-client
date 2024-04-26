9e881b530b867b3e1d6a69d16749e1bb03212a7c
msahrana <msahrana@gmail.com>
2024-04-26 23:22:19 +0600

    register page finished

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,10 +1,76 @@
 import {Link} from "react-router-dom";
+import useAuth from "../../hooks/useAuth/useAuth";
+import {useForm} from "react-hook-form";
+import {useState} from "react";
+import Swal from "sweetalert2";
 
 const Register = () => {
+  const {createUser, user, setUser, updateUser} = useAuth() || {};
+  const [error, setError] = useState("");
+
+  const {
+    register,
+    handleSubmit,
+    formState: {errors},
+  } = useForm();
+
+  const onSubmit = (data) => {
+    const {name, photoURL, email, password} = data;
+    setError("");
+    /* password authentication */
+    if (password.length < 8) {
+      setError("Password should be at least 8 characters");
+      return;
+    } else if (
+      !/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/.test(
+        password
+      )
+    ) {
+      setError(
+        "Password should have one uppercase, one lowercase, one digit & one special character"
+      );
+      return;
+    }
+    /* user create */
+    createUser(email, password)
+      .then((result) => {
+        /* for server */
+        const newUser = {name, photoURL, email, password};
+        fetch("http://localhost:5000/user", {
+          method: "POST",
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify(newUser),
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.insertedId) {
+              Swal.fire({
+                position: "top-end",
+                icon: "success",
+                title: "User create successfully!",
+                showConfirmButton: false,
+                timer: 1500,
+              });
+            }
+          });
+        /* update user */
+        updateUser(name, photoURL).then(() => {
+          console.log(result.user);
+          setUser({...user, displayName: name, photoURL: photoURL});
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        setError(err.massage);
+      });
+  };
+
   return (
     <div className="w-full max-w-md p-8 space-y-3 rounded-xl border-2 mx-auto">
       <h1 className="text-2xl font-bold text-center">Please Register</h1>
-      <form noValidate="" action="" className="space-y-6">
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
         <div className="space-y-1 text-sm">
           <label htmlFor="name" className="block">
             Name
@@ -15,7 +81,11 @@ const Register = () => {
             id="name"
             placeholder="Your Name Here"
             className="w-full px-4 py-3 rounded-md border"
+            {...register("name", {required: true})}
           />
+          {errors.name && (
+            <span className="text-red-500">Name is required</span>
+          )}
         </div>
         <div className="space-y-1 text-sm">
           <label htmlFor="photoURL" className="block">
@@ -27,7 +97,11 @@ const Register = () => {
             id="photoURL"
             placeholder="PhotoURL Here"
             className="w-full px-4 py-3 rounded-md border"
+            {...register("photoURL", {required: true})}
           />
+          {errors.photoURL && (
+            <span className="text-red-500">PhotoURL is required</span>
+          )}
         </div>
         <div className="space-y-1 text-sm">
           <label htmlFor="email" className="block">
@@ -39,7 +113,11 @@ const Register = () => {
             id="email"
             placeholder="Your Email Here"
             className="w-full px-4 py-3 rounded-md border"
+            {...register("email", {required: true})}
           />
+          {errors.email && (
+            <span className="text-red-500">Email is required</span>
+          )}
         </div>
         <div className="space-y-1 text-sm">
           <label htmlFor="password" className="block">
@@ -51,12 +129,17 @@ const Register = () => {
             id="password"
             placeholder="Password"
             className="w-full px-4 py-3 rounded-md border"
+            {...register("password", {required: true})}
           />
+          {errors.password && (
+            <span className="text-red-500">Password is required</span>
+          )}
         </div>
         <button className="block w-full p-3 text-center rounded-sm bg-orange-500 text-xl">
           Register
         </button>
       </form>
+      {error && <p className="text-red-600">{error}</p>}
       <div className="flex items-center pt-4 space-x-1">
         <div className="flex-1 h-px sm:w-16 dark:bg-gray-300"></div>
         <p className="px-3 text-xm dark:text-gray-600">
--- src/shared/Navbar/Navbar.jsx
+++ src/shared/Navbar/Navbar.jsx
@@ -65,7 +65,9 @@ const Navbar = () => {
             {navLinks}
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">Rana Tourism Economics</a>
+        <Link to="/" className="btn btn-ghost text-xl">
+          Rana Tourism Economics
+        </Link>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">{navLinks}</ul>
