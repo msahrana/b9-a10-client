3ff1574429df98665c7a1333bacd0a9cf6a051f5
msahrana <msahrana@gmail.com>
2024-04-28 10:19:59 +0600

    sociallogin page added & setup successfully

--- src/components/DetailsPage/DetailsPage.jsx
+++ src/components/DetailsPage/DetailsPage.jsx
@@ -1,7 +1,26 @@
+import {Link} from "react-router-dom";
+
 const DetailsPage = () => {
   return (
-    <div>
-      <h1>Details Page here</h1>
+    <div className="card w-2/3 mx-auto mt-6 mb-10 bg-base-100 shadow-xl">
+      <figure>
+        <img
+          src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+          alt="Shoes"
+        />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">Shoes!</h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div className="card-actions justify-end">
+          <Link
+            className="bg-red-500 text-xl text-white px-3 py-1 rounded-lg"
+            to="/"
+          >
+            Back to Home
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
