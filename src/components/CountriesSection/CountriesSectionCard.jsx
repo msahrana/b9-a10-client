10d8c963a15d487a52ebc65f35096d9eb62d93c1
msahrana <msahrana@gmail.com>
2024-04-30 00:47:07 +0600

    fire base file edit

--- src/components/CountriesSection/CountriesSectionCard.jsx
+++ src/components/CountriesSection/CountriesSectionCard.jsx
@@ -1,18 +1,26 @@
 import {Link} from "react-router-dom";
 
 const CountriesSectionCard = ({item}) => {
-  const {photoURL, country} = item || {};
+  const {photoURL, country, description} = item || {};
 
   return (
     <Link to={`/countryList/${country}`}>
-      <div className="w-full bg-base-100 shadow-xl">
+      <div className="w-full bg-base-100 shadow-xl rounded-2xl">
         <figure>
           <img
-            className="w-full h-full rounded-2xl"
+            className="w-full h-[360px] rounded-2xl"
             src={photoURL}
             alt="photo"
           />
         </figure>
+        <h1 className="text-3xl text-blue-500 font-bold uppercase py-4">
+          <span className="text-green-600 mr-40 pl-16">Country Name: </span>
+          {country}
+        </h1>
+        <p className="p-4 text-justify">
+          <span className="font-bold mr-4">Description:</span>
+          {description}
+        </p>
       </div>
     </Link>
   );
--- src/firebase/firebase.Config/firebase.Config.js
+++ src/firebase/firebase.Config/firebase.Config.js
@@ -5,12 +5,12 @@ import {initializeApp} from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyADdQ1QrjxatVa6Sm2tuuipg-ODD0iPnac",
-  authDomain: "b9-a10-4a8d1.firebaseapp.com",
-  projectId: "b9-a10-4a8d1",
-  storageBucket: "b9-a10-4a8d1.appspot.com",
-  messagingSenderId: "362283598624",
-  appId: "1:362283598624:web:e06e615f4a137679e08d45",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
